c8fe162e6e75d107515cb93510cbe46576baa780
Rendy Irawana <rendyakun50@gmail.com>
2025-07-21 09:53:40 +0700

    updated

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,12 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    "extends": ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
+    "rules": {
+      "@typescript-eslint/no-explicit-any": "off"
+    }
+  } 
 ];
 
 export default eslintConfig;
